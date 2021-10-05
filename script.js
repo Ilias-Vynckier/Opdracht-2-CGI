808ce195492857b8e2bec6389f1e8bbcad4d2b81
Ilias Vynckier <ilias.vynckier@gmail.com>
2021-10-05 16:51:28 +0200

    Fetch fils table

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-fetch('http://ilias/data.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
+fetch("http://ilias/data.json")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+$(document).ready(function () {
+  // FETCHING DATA FROM JSON FILE
+  $.getJSON("http://ilias/data.json", function (data) {
+    var student = "";
+
+    // ITERATING THROUGH OBJECTS
+    $.each(data, function (key, value) {
+      //CONSTRUCTION OF ROWS HAVING
+      // DATA FROM JSON OBJECT
+      student += "<tr>";
+      student += "<td>" + value.name + "</td>";
+
+      student += "<td>" + value.dink + "</td>";
+      student += "</tr>";
+    });
+
+    //INSERTING ROWS INTO TABLE
+    $("#test").append(student);
+  });
+});
