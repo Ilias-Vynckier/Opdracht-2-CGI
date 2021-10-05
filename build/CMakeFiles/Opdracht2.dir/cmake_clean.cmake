3f4f4d62fa71e97d31c506fc5c471b5993a84907
Ilias Vynckier <ilias.vynckier@gmail.com>
2021-10-05 16:58:23 +0200

    added cmake

---
+++ build/CMakeFiles/Opdracht2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Opdracht2.dir/main.cpp.o"
+  "Opdracht2"
+  "Opdracht2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Opdracht2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
