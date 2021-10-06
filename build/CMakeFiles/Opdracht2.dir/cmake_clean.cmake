bc1e6ed66ad98dd5fd284b2971ea2400e92f8a5b
Ilias Vynckier <ilias.vynckier@gmail.com>
2021-10-06 18:45:31 +0200

    able to process POST and appand data to file

--- build/CMakeFiles/Opdracht2.dir/cmake_clean.cmake
+++ build/CMakeFiles/Opdracht2.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Opdracht2.dir/main.cpp.o"
+  "CMakeFiles/Opdracht2.dir/main.c.o"
   "Opdracht2"
   "Opdracht2.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C)
   include(CMakeFiles/Opdracht2.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
